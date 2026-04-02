1dab381ce058b757ef963a5f2f99d2541437b017
Gabriel Anjos <gabrielalvares5000@gmail.com>
2026-04-02 15:28:41 +0000

    Nível Mestre: comparação com múltiplos atributos

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -2,52 +2,56 @@
 
 int main() {
 
-    int opcao;
+    int op1, op2;
 
-    // Dados fixos
     char nome1[] = "São Paulo";
     char nome2[] = "Rio de Janeiro";
 
     int populacao1 = 12300000, populacao2 = 6000000;
     float area1 = 1521.11, area2 = 1200.25;
     float pib1 = 699.28, pib2 = 300.50;
-    int pontos1 = 50, pontos2 = 30;
 
     float densidade1 = populacao1 / area1;
     float densidade2 = populacao2 / area2;
 
-    // MENU
-    printf("Escolha o atributo:\n");
-    printf("1 - População\n2 - Área\n3 - PIB\n4 - Pontos Turísticos\n5 - Densidade\n");
-    scanf("%d", &opcao);
+    float valor1_c1, valor1_c2;
+    float valor2_c1, valor2_c2;
 
-    printf("\nResultado:\n");
+    // MENU 1
+    printf("Escolha o primeiro atributo:\n1-População\n2-Área\n3-PIB\n4-Densidade\n");
+    scanf("%d", &op1);
 
-    switch(opcao) {
+    // MENU 2
+    printf("Escolha o segundo atributo:\n1-População\n2-Área\n3-PIB\n4-Densidade\n");
+    scanf("%d", &op2);
 
-        case 1:
-            printf("População\n");
-            if (populacao1 > populacao2)
-                printf("Carta 1 venceu!\n");
-            else if (populacao1 < populacao2)
-                printf("Carta 2 venceu!\n");
-            else
-                printf("Empate!\n");
-            break;
-
-        case 5:
-            printf("Densidade\n");
-            if (densidade1 < densidade2)
-                printf("Carta 1 venceu!\n");
-            else if (densidade1 > densidade2)
-                printf("Carta 2 venceu!\n");
-            else
-                printf("Empate!\n");
-            break;
-
-        default:
-            printf("Opção inválida!\n");
+    // Atributo 1
+    switch(op1) {
+        case 1: valor1_c1 = populacao1; valor1_c2 = populacao2; break;
+        case 2: valor1_c1 = area1; valor1_c2 = area2; break;
+        case 3: valor1_c1 = pib1; valor1_c2 = pib2; break;
+        case 4: valor1_c1 = densidade1; valor1_c2 = densidade2; break;
     }
+
+    // Atributo 2
+    switch(op2) {
+        case 1: valor2_c1 = populacao1; valor2_c2 = populacao2; break;
+        case 2: valor2_c1 = area1; valor2_c2 = area2; break;
+        case 3: valor2_c1 = pib1; valor2_c2 = pib2; break;
+        case 4: valor2_c1 = densidade1; valor2_c2 = densidade2; break;
+    }
+
+    float soma1 = valor1_c1 + valor2_c1;
+    float soma2 = valor1_c2 + valor2_c2;
+
+    printf("\nResultado Final:\n");
+
+    if (soma1 > soma2)
+        printf("Carta 1 venceu!\n");
+    else if (soma2 > soma1)
+        printf("Carta 2 venceu!\n");
+    else
+        printf("Empate!\n");
 
     return 0;
 }
