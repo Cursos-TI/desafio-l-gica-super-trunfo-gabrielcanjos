316290684fcdf252a380e5bf8258ee6deff51c84
Gabriel Anjos <gabrielalvares5000@gmail.com>
2026-04-02 15:27:43 +0000

    Nível Aventureiro: menu interativo com switch

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -2,37 +2,51 @@
 
 int main() {
 
-    // Carta 1
-    char nome1[50] = "São Paulo";
-    char estado1[3] = "SP";
-    int populacao1 = 12300000;
-    float area1 = 1521.11;
-    float pib1 = 699.28;
+    int opcao;
 
-    // Carta 2
-    char nome2[50] = "Rio de Janeiro";
-    char estado2[3] = "RJ";
-    int populacao2 = 6000000;
-    float area2 = 1200.25;
-    float pib2 = 300.50;
+    // Dados fixos
+    char nome1[] = "São Paulo";
+    char nome2[] = "Rio de Janeiro";
 
-    // Cálculos
+    int populacao1 = 12300000, populacao2 = 6000000;
+    float area1 = 1521.11, area2 = 1200.25;
+    float pib1 = 699.28, pib2 = 300.50;
+    int pontos1 = 50, pontos2 = 30;
+
     float densidade1 = populacao1 / area1;
     float densidade2 = populacao2 / area2;
 
-    float pibPerCapita1 = pib1 / populacao1;
-    float pibPerCapita2 = pib2 / populacao2;
+    // MENU
+    printf("Escolha o atributo:\n");
+    printf("1 - População\n2 - Área\n3 - PIB\n4 - Pontos Turísticos\n5 - Densidade\n");
+    scanf("%d", &opcao);
 
-    // Comparação (POPULAÇÃO)
-    printf("Comparação de cartas (Atributo: População)\n\n");
+    printf("\nResultado:\n");
 
-    printf("Carta 1 - %s (%s): %d\n", nome1, estado1, populacao1);
-    printf("Carta 2 - %s (%s): %d\n", nome2, estado2, populacao2);
+    switch(opcao) {
 
-    if (populacao1 > populacao2) {
-        printf("\nResultado: Carta 1 (%s) venceu!\n", nome1);
-    } else {
-        printf("\nResultado: Carta 2 (%s) venceu!\n", nome2);
+        case 1:
+            printf("População\n");
+            if (populacao1 > populacao2)
+                printf("Carta 1 venceu!\n");
+            else if (populacao1 < populacao2)
+                printf("Carta 2 venceu!\n");
+            else
+                printf("Empate!\n");
+            break;
+
+        case 5:
+            printf("Densidade\n");
+            if (densidade1 < densidade2)
+                printf("Carta 1 venceu!\n");
+            else if (densidade1 > densidade2)
+                printf("Carta 2 venceu!\n");
+            else
+                printf("Empate!\n");
+            break;
+
+        default:
+            printf("Opção inválida!\n");
     }
 
     return 0;
